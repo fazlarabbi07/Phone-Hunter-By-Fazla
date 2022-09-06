5b5c9fa1ae87a3da6950e11d65c679a54af5a6c4
fazlarabbi07 <md.fazlarabbi07@gmail.com>
2022-09-07 01:19:04 +0600

    details functionality works fine

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ const loadAllMobileData=(userFieldValue)=>{
 }
 
 const displayAllPhoneData=(allPhoneDatas)=>{
+    
+    
     // For Wrong User Input 
     const noSerchFound=document.getElementById('noSearchResult'); 
     if(allPhoneDatas.length==0)
@@ -57,7 +59,7 @@ const displayAllPhoneData=(allPhoneDatas)=>{
     }
     // Foreach Loop
     allPhoneDatas.forEach(allPhoneData => {
-        console.log(allPhoneData);
+        
         const allMobileChild=document.createElement('div');  
         const allMobileParent=document.getElementById('displayUserSearch');
     
@@ -70,7 +72,7 @@ const displayAllPhoneData=(allPhoneDatas)=>{
                   <h5 class="card-title">${allPhoneData.phone_name}</h5>
                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
-                <div><button onclick="btnDetails('${allPhoneData.slug}')" class="btn btn-primary px-4  m-3 w-50 fs-5">Details</button></div>
+                <div><button onclick="btnDetails('${allPhoneData.slug}')" class="btn btn-primary px-4  m-3 w-50 fs-5" data-bs-toggle="modal" data-bs-target="#detailsBtnModal">Details</button></div>
               </div>
 
     
@@ -80,9 +82,7 @@ const displayAllPhoneData=(allPhoneDatas)=>{
         
         
     });
-    
-    
-    
+  
 }
 // Each Btn Details click
 const btnDetails=(slugId)=>{
@@ -91,7 +91,7 @@ const btnDetails=(slugId)=>{
     fetch(`https://openapi.programming-hero.com/api/phone/${specificSlug}`)
     .then(response=>response.json())
     .then(data=>eachMobile(data.data))
-    // data.data.image
+    //data.data.image
     //data.data.brand
     //data.data.mainFeatures.storage
     //data.data.mainFeatures.displaySize
@@ -104,5 +104,23 @@ const btnDetails=(slugId)=>{
 
 }
 const eachMobile=(doubleData)=>{
-    console.log(doubleData.brand);
+    //Phone Name
+    console.log(doubleData);
+    const detailsBtnModalLabel=document.getElementById('detailsBtnModalLabel');
+    detailsBtnModalLabel.innerText=doubleData.name;
+    const modalBody=document.getElementById('modalBody');
+    const modalChild=document.createElement('div');
+    modalBody.innerHTML=``; //Crearing Previous loded data
+    modalChild.innerHTML=`<img class=" container img img-fluid " src="${doubleData.image}" alt="">
+    <h3>Brand Name:${doubleData.brand}</h3>
+    <p> <h5>Sorage:</h5> ${doubleData.mainFeatures.storage}</p>
+    <p><h6>Display Size:</h6>${doubleData.mainFeatures.displaySize}</p>
+    <p><h6>Chipset:</h6>${doubleData.mainFeatures.chipSet}</p>
+    <p><h6>Memory:</h6>${doubleData.mainFeatures.memory}</p>
+    <p><h6>Sensors:</h6>${doubleData.mainFeatures.sensors}</p>
+    <p><h6>Storage:</h6>${doubleData.mainFeatures.storage}</p>
+    <p><h6>Release Date:</h6>${doubleData.releaseDate ? doubleData.releaseDate :"No date found"}</p>`;
+    modalBody.appendChild(modalChild);
+
 }
+{/* <p>Storage:${doubleData.name.mainFeatures.storage}</p> */}
